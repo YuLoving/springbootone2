28bcbc8c0c902ccdb18369e00d0c05dc1778b807
YuLoving <974456795@qq.com>
2019-04-10 14:42:12 +0800

    添加几个http传输的工具类

---
+++ src/main/java/cn/nj/springbootone/utils/http/HttpClientUtil.java
@@ -0,0 +1,126 @@
+package cn.nj.springbootone.utils.http;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * http请求的工具类
+ * 
+ */
+public class HttpClientUtil
+{
+	private static final Logger logger = LoggerFactory.getLogger(HttpClientUtil.class);
+
+	@SuppressWarnings("deprecation")
+	private static HttpClient hc = new DefaultHttpClient();
+
+	/**
+	 * // Post请求
+	 * @param url
+	 * @param params
+	 * @return
+	 */
+	public static String post(String url, List<NameValuePair> params)
+	{
+		String body = null;
+		try
+		{
+			// Post请求
+			HttpPost httppost = new HttpPost(url);
+			// 设置参数
+			httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+			// 发送请求
+			HttpResponse httpresponse = hc.execute(httppost);
+			// 获取返回数据
+			HttpEntity entity = httpresponse.getEntity();
+			body = EntityUtils.toString(entity);
+			if (entity != null)
+			{
+				entity.consumeContent();
+			}
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			e.printStackTrace();
+		}
+		catch (ClientProtocolException e)
+		{
+			e.printStackTrace();
+		}
+		catch (ParseException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return body;
+	}
+
+	public static String get(String url,String param)
+	{
+		String result = "";
+		BufferedReader in = null;
+		try {
+			String urlNameString = url + "?" + param;
+			URL realUrl = new URL(urlNameString);
+			// 打开和URL之间的连接
+			URLConnection connection = realUrl.openConnection();
+			// 设置通用的请求属性
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("user-agent",
+					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			// 建立实际的连接
+			connection.connect();
+			// 获取所有响应头字段
+			Map<String, List<String>> map = connection.getHeaderFields();
+			// 遍历所有的响应头字段
+			//for (String key : map.keySet()) {
+			//	System.out.println(key + "--->" + map.get(key));
+			//}
+			// 定义 BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送GET请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输入流
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result;
+	}
+}
